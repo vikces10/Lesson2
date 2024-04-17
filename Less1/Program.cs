7b9c643bf1e8a793c4d421aec1ae9160ed6f6370
Viktor Samoilenko <vikces10@gmail.com>
2024-04-17 19:11:15 +0300

    Последний коммит

--- Less1/Program.cs
+++ Less1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿
 
 Console.WriteLine("Hello, World!");
 
--- Less1/obj/Debug/net8.0/Less1.AssemblyInfo.cs
+++ Less1/obj/Debug/net8.0/Less1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Less1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5ef953bb06ebb34685801561f319cf425d14a6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d616bb17b3f1e707f95f0943782e5cdaa238bed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Less1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Less1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
