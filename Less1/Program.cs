585a223804803869638f9e9512b8cfcf63203286
Viktor Samoilenko <vikces10@gmail.com>
2024-04-17 18:19:04 +0300

    Добавили несколько переменных

--- Less1/Program.cs
+++ Less1/Program.cs
@@ -1,1 +1,11 @@
 ﻿Console.WriteLine("Hello, World!");
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
--- Less1/obj/Debug/net8.0/Less1.AssemblyInfo.cs
+++ Less1/obj/Debug/net8.0/Less1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Less1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+931ef80a38905cae92ed26aeeb9d6f1684e111c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9adc30d3af53a7e95709ac39b6e680e193a688f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Less1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Less1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
