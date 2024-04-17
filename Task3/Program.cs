f6c1f5259b508af3ad2e30afe9c83c2b55cd717a
Viktor Samoilenko <vikces10@gmail.com>
2024-04-17 19:57:49 +0300

    Добавили задачу вывода чисел от 1 до N

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -37,3 +37,15 @@ if (e > max)
     max = e;
 }
 Console.WriteLine(max);
+
+// Выводит все числа от 1 до N:
+int n = 100;
+int i = 1;
+
+while (i <= n)
+{
+    //Console.WriteLine(i);
+    Console.Write(i);
+    Console.Write(' ');
+    i = i + 1;
+}
--- Task3/obj/Debug/net8.0/Task3.AssemblyInfo.cs
+++ Task3/obj/Debug/net8.0/Task3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65957a268690556264673aee06773dfed1e07f78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8e238ff3c7a808a8ba6f048d54628afa6704bde")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
